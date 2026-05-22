66052d0d3095d9efd0041811a04f2ae8ddb7c8e8
tanvir <tanvirulislam149@gmail.com>
2026-05-22 22:41:00 +0600

    tour_project_init

--- src/User/userInterface.ts
+++
@@ -1,8 +0,0 @@
-export default interface IUser {
-  name: string;
-  email: string;
-  password: string;
-  phone?: number; // Optional fields
-  address?: string;
-  role: "Admin" | "Staff" | "User";
-}
