001a79047ea0f9a27526cb14899e7444716bed6f
tanvir <tanvirulislam149@gmail.com>
2026-05-22 23:54:52 +0600

    env done

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const startServer = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ph_tour");
+    await mongoose.connect(process.env.DB_URI as string);
 
     console.log("Connected to MongoDB");
 
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server is running on port " + (process.env.PORT || 5000));
     });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
