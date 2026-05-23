256742ea0b399c7bef3e8bff167cb580dd3b0dc5
tanvir <tanvirulislam149@gmail.com>
2026-05-23 22:55:13 +0600

    user route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
+import { userRouter } from "./app/modules/user/user.route";
+import cors from "cors";
 
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1/user/", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to PH_tour API" });
