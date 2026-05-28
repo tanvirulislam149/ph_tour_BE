7ab29213d400fc2c3083dc15b768d4e74cf23c9e
tanvir <tanvirulislam149@gmail.com>
2026-05-28 15:42:20 +0600

    all user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import User from "./user.model";
 import httpstatus from "http-status-codes";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await userService.createUser(req.body);
 
     res.status(httpstatus.CREATED).json({
@@ -12,12 +13,22 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       message: "User created successfully",
       user: user,
     });
-  } catch (error: any) {
-    console.log("From controller", error);
-    next(error);
-  }
-};
+  },
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userService.getAllUsersService();
+
+    res.status(httpstatus.OK).json({
+      success: true,
+      message: "Users retrieved successfully",
+      users: users,
+    });
+  },
+);
 
 export const userController = {
   createUser,
+  getAllUsers,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,6 +7,12 @@ const createUser = async (payload: Partial<IUser>) => {
   return user;
 };
 
+const getAllUsersService = async () => {
+  const users = await User.find();
+  return users;
+};
+
 export const userService = {
   createUser,
+  getAllUsersService,
 };
